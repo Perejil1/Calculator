57098c486eca63e996aa2893d63386220dd3deec
Perejil1 <163358150+Perejil1@users.noreply.github.com>
2024-08-13 17:46:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const agregar = (valor) => {
-  document.getElementById('pantalla').value += valor;
+    document.getElementById('pantalla').value += valor;
 }
 
 const borrar = () => {
-  document.getElementById('pantalla').value = '';
+    document.getElementById('pantalla').value = '';
 }
 
 const calcular = () => {
-  const valorPantalla = document.getElementById('pantalla');
-  const resultado = eval(valorPantalla);
-  document.getElementById('pantalla').value = resultado.value;
-  
+    try {
+        const valorPantalla = document.getElementById('pantalla').value;
+        const resultado = eval(valorPantalla);
+        document.getElementById('pantalla').value = resultado;
+    } catch (error) {
+        document.getElementById('pantalla').value = 'Error';
+    }
 }
