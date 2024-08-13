8cd305c36e5dc97301ee1da2b98ec6158cd88b12
Perejil1 <163358150+Perejil1@users.noreply.github.com>
2024-08-13 16:27:24 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const agregar = (valor) => {
+  document.getElementById('pantalla').value += valor;
+}
+
+const borrar = () => {
+  document.getElementById('pantalla').value = '';
+}
+
+const calcular = () => {
+  const valorPantalla = document.getElementById('pantalla');
+  const resultado = eval(valorPantalla);
+  document.getElementById('pantalla').value = resultado.value;
+  
+}
